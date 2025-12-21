f55680dc8abe66150128f4994ac26d3b041d8ad6
Michael Dudzik <mkdudik15@gmail.com>
2025-12-22 01:21:10 +0300

    feat:create input component

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Input.css';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  label: string;
+  icon?: React.ReactNode;
+  error?: string;
+}
+
+export const Input: React.FC<Props> = ({
+  id,
+  label,
+  icon,
+  error,
+  className = '',
+  ...props
+}) => {
+  return (
+    <fieldset className={`auth-field ${className}`}>
+      <label htmlFor={id} className="auth-label">
+        {icon && <span className="label-icon">{icon}</span>}
+        {label}
+      </label>
+
+      <div className={`input-wrapper ${error ? 'has-error' : ''}`}>
+        <input id={id} className="auth-input" {...props} />
+      </div>
+
+      {error && <span className="error-message">{error}</span>}
+    </fieldset>
+  );
+};
