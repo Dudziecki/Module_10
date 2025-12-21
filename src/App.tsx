e87b858b0bebec2ab8ec44298727148b080e7ddf
Michael Dudzik <mkdudik15@gmail.com>
2025-12-22 00:45:56 +0300

    feat:Implement main App structure with routing and context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { AuthProvider } from './contexts/AuthContext';
+import { RouterSetup } from './RouterSetup';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <div className="App">
+          <Header />
+          <RouterSetup />
+
+          <Footer text={'© 2024 sidekick'} />
+        </div>
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
