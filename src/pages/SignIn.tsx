546254f6b54d5ce3cd1670ac6ad506f968b3af90
Michael Dudzik <mkdudik15@gmail.com>
2025-12-22 01:01:40 +0300

    feat:create Sign In and Sign Up pages

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { AuthLayout } from '../components/AuthLayout/AuthLayout';
+import { Form } from '../components/common/Form/Form';
+import { Input } from '../components/common/Input/Input';
+import { Button } from '../components/common/Button/Button';
+import { EmailIcon } from '../assets/icons/EmailIcon';
+import { PasswordIcon } from '../assets/icons/PasswordIcon';
+import { Footer } from '../components/Footer/Footer';
+import { useAuth } from '../contexts/AuthContext';
+import { AuthService } from '../services/auth.service';
+
+export const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const user = AuthService.signIn(email, password);
+      login(user);
+      navigate('/');
+    } catch (err) {
+      setError((err as Error).message);
+    }
+  };
+
+  return (
+    <AuthLayout>
+      <Form
+        title="Sign in into an account"
+        subtitle="Enter your email and password to sign in into this app"
+        onSubmit={handleSubmit}
+      >
+        <Input
+          id="email"
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter email"
+          icon={<EmailIcon />}
+        />
+
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter password"
+          icon={<PasswordIcon />}
+        />
+
+        <Button type="submit">Sign In</Button>
+      </Form>
+
+      <div className="terms-container">
+        <p>
+          Forgot to create an account?
+          <Link to="/signup" className="helper-link">
+            {' '}
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </AuthLayout>
+  );
+};
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { AuthLayout } from '../components/AuthLayout/AuthLayout';
+import { Form } from '../components/common/Form/Form';
+import { Input } from '../components/common/Input/Input';
+import { Button } from '../components/common/Button/Button';
+import { EmailIcon } from '../assets/icons/EmailIcon';
+import { PasswordIcon } from '../assets/icons/PasswordIcon';
+import '../components/AuthLayout/AuthLayout.css';
+import { useAuth } from '../contexts/AuthContext';
+import { AuthService } from '../services/auth.service';
+
+export const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const user = AuthService.signUp(email, password);
+      login(user);
+      navigate('/');
+    } catch (err) {
+      setError((err as Error).message);
+    }
+  };
+
+  return (
+    <AuthLayout>
+      <Form
+        title="Create an account"
+        subtitle="Enter your email and password to sign up into this app"
+        onSubmit={handleSubmit}
+      >
+        <Input
+          id="email"
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter email"
+          icon={<EmailIcon />}
+        />
+
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter password"
+          icon={<PasswordIcon />}
+        />
+
+        <Button type="submit">Sign Up</Button>
+      </Form>
+
+      <div className="terms-container">
+        <p className="terms-text">
+          By clicking continue, you agree to our
+          <span> Terms of Service</span> and <span>Privacy</span>
+        </p>
+        <p>
+          Already have an account?
+          <Link to="/signin" className="helper-link">
+            {' '}
+            Sign in
+          </Link>
+        </p>
+      </div>
+
+      {/*{error && <Notification message={error} />}*/}
+    </AuthLayout>
+  );
+};
