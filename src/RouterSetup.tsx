74b39874856859adf1c01abc24ba828d082b9485
Michael Dudzik <mkdudik15@gmail.com>
2025-12-22 00:50:35 +0300

    feat:create component for Routes

---
+++ src/RouterSetup.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+import { useAuth } from './contexts/AuthContext';
+
+import { Home } from './pages/Home';
+import { SignUp } from './pages/SignUp';
+import { PageNotFound } from './pages/PageNotFound';
+import { SignIn } from './pages/SignIn';
+import { Profile } from './pages/Profile';
+
+import './App.css';
+
+export const RouterSetup: React.FC = () => {
+  const { user } = useAuth();
+
+  return (
+    <>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/pagenotfound" element={<PageNotFound />} />
+          <Route
+            path="/profile"
+            element={user ? <Profile /> : <Navigate to="/signin" replace />}
+          />
+
+          <Route path="/*" element={<Navigate to="/pagenotfound" replace />} />
+        </Routes>
+      </main>
+    </>
+  );
+};
