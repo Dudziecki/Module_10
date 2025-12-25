dc7d86d03c15dd9309f15b8401edea14df56374b
Michael Dudzik <mkdudik15@gmail.com>
2025-12-25 17:49:21 +0300

    feat:create Footer element

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactNode } from 'react';
+import './Footer.css';
+
+type FooterPropsType = {
+  children: ReactNode;
+};
+
+export const Footer: FC<FooterPropsType> = ({ children }) => {
+  return <footer className="footer">{children}</footer>;
+};
