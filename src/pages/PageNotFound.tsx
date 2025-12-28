00da3e7e90abf97af1e5e513632b89b520e3a197
Michael Dudzik <mkdudik15@gmail.com>
2025-12-28 19:54:36 +0300

    feat:create not found page

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,14 @@
+import { PageNotFoundIcon } from '../assets/icons/PageNotFoundIcon';
+import './PageNotFound.css';
+import { FC } from 'react';
+
+export const PageNotFound: FC = () => {
+  return (
+    <section className="page-not-found">
+      <div className="wrapper">
+        <PageNotFoundIcon />
+        <h2>Page Not Found</h2>
+      </div>
+    </section>
+  );
+};
