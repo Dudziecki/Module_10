279b000ce7bfd40eae31533a8d2632ef6804e7a4
Michael Dudzik <mkdudik15@gmail.com>
2025-12-28 20:48:37 +0300

    feat:add post component

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,98 @@
+import React, { useReducer, useState } from "react";
+import { IAuthor, IComment } from "./types";
+import { PostHeader } from "./PostHeader/PostHeader";
+import { LikeButton } from "./LikeButton/LikeButton";
+import { PostContent } from "./PostContent/PostContent";
+import "./Post.css";
+import { MessageIcon } from "../../assets/icons/MessageIcon";
+import { ShowCommentIcon } from "../../assets/icons/ShowCommentIcon";
+import { CommentItem } from "./CommentItem/CommentItem";
+import { CommentForm } from "./CommentForm/CommentForm";
+import {
+  addCommentAC,
+  commentsReducer,
+  deleteCommentAC,
+} from "../../store/comments-reducer";
+
+type PostPropsType = {
+  author: IAuthor;
+  createdAt: string;
+  description: string;
+  image?: string;
+  likesCount: number;
+  comments: IComment[];
+  isLoggedIn: boolean;
+};
+
+export const Post: React.FC<PostPropsType> = ({
+  description,
+  image,
+  isLoggedIn,
+  createdAt,
+  likesCount,
+  author,
+  ...props
+}) => {
+  const [isCommentsOpen, setIsCommentsOpen] = useState(false);
+  const [comments, dispatch] = useReducer(commentsReducer, props.comments);
+
+  const handleAddComment = (text: string) => {
+    dispatch(addCommentAC(text));
+  };
+
+  const handleRemoveComment = (id: number) => {
+    dispatch(deleteCommentAC(id));
+  };
+
+  return (
+    <article className="post">
+      <PostHeader author={author} createdAt={createdAt} />
+
+      <PostContent image={image} description={description} />
+
+      <div className="post-actions-section">
+        <div className="post-actions">
+          <LikeButton count={likesCount} />
+
+          <button
+            onClick={() => setIsCommentsOpen(!isCommentsOpen)}
+            className="comments-toggle"
+            aria-expanded={isCommentsOpen}
+            aria-label={`${isCommentsOpen ? "Close" : "Open"} comments`}
+          >
+            <span className="comments-count">
+              <MessageIcon /> {comments.length}{" "}
+              {isLoggedIn
+                ? "comments"
+                : "You have to login to see the comments"}
+            </span>
+            <span className="toggle-icon">
+              {isLoggedIn && (
+                <ShowCommentIcon shouldShowComments={isCommentsOpen} />
+              )}
+            </span>
+          </button>
+        </div>
+
+        <section>
+          {isCommentsOpen &&
+            (isLoggedIn ? (
+              <ul className="post-list">
+                {comments.map((comment) => (
+                  <CommentItem
+                    key={comment.id}
+                    comment={comment}
+                    onDelete={handleRemoveComment}
+                  />
+                ))}
+              </ul>
+            ) : (
+              <></>
+            ))}
+
+          <CommentForm onAdd={handleAddComment} />
+        </section>
+      </div>
+    </article>
+  );
+};
