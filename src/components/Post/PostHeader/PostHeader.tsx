6e027f6c2edf06b1fcb457523e791686238db387
Michael Dudzik <mkdudik15@gmail.com>
2025-12-29 13:16:43 +0300

    feat:create post header component

---
+++ src/components/Post/PostHeader/PostHeader.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { IAuthor } from "../types";
+import { DEFAULT_USER } from "../../../lib/constants/constants";
+import "./PostHeader.css";
+
+type HeaderPropsType = {
+  author: IAuthor;
+  createdAt: string;
+};
+
+export const PostHeader: React.FC<HeaderPropsType> = ({
+  author,
+  createdAt,
+}) => (
+  <section className="post-header">
+    <img
+      src={author.avatar || DEFAULT_USER.avatar}
+      alt={`${author.name}'s profile picture`}
+      className="post-header-avatar"
+    />
+    <div className="post-header-info">
+      <h3 className="post-header-name">{author.name}</h3>
+      <time dateTime={createdAt} className="post-header-timestamp">
+        {createdAt}
+      </time>
+    </div>
+  </section>
+);
