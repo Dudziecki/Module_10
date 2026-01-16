92d5e308caf426f3df7c4c9785f11278a23d7101
Michael Dudzik <mkdudik15@gmail.com>
2026-01-16 16:15:23 +0300

    fix:remove index and move out initial array

--- src/components/SuggestedPeopleSection/SuggestedPeopleSection.tsx
+++ src/components/SuggestedPeopleSection/SuggestedPeopleSection.tsx
@@ -1,48 +1,16 @@
 import React from "react";
 import { SectionItem } from "../SuggestedPeopleItem/SectionItem";
 import "./SuggestedPeopleSection.css";
+import { suggestedPeople } from "../../lib/SuggestedPeople/SuggestedPeople";
 
 export const SuggestedPeopleSection = () => {
-  const suggestedPeople = [
-    {
-      name: "Helena",
-      link: "@helenahills",
-      avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgPimc_RQYYbUhV3A_xER8GPifFju7nveLA&s",
-    },
-    {
-      name: "Charles",
-      link: "@charles",
-      avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgPimc_RQYYbUhV3A_xER8GPifFju7nveLA&s",
-    },
-    {
-      name: "Oscar Davis",
-      link: "@oscardavis",
-      avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgPimc_RQYYbUhV3A_xER8GPifFju7nveLA&s",
-    },
-    {
-      name: "Daniel Jay Park",
-      link: "@danielj",
-      avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgPimc_RQYYbUhV3A_xER8GPifFju7nveLA&s",
-    },
-    {
-      name: "Carlo Rojas",
-      link: "@carlorojas",
-      avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgPimc_RQYYbUhV3A_xER8GPifFju7nveLA&s",
-    },
-  ];
-
   return (
     <section className="suggested-people-section">
       <h2>Suggested people</h2>
       <div className="section-list">
-        {suggestedPeople.map((user, index) => (
+        {suggestedPeople.map((user) => (
           <SectionItem
-            key={index}
+            key={user.id}
             title={user.name}
             subtitle={user.link}
             avatar={user.avatar}
